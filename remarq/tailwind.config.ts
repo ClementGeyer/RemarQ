69c8c71f4d7e56c10ea97ec2e0124278faeca14e
Clément Geyer <clementgeyer4@gmail.com>
2024-03-26 19:17:11 +0100

    adding colors

--- remarq/tailwind.config.ts
+++ remarq/tailwind.config.ts
@@ -7,6 +7,14 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "primary": "#344E41",
+      "primary-light": "#3A5A40",
+      "primary-lighter": "#588157",
+      
+      "secondary": "#DAD7CD",
+      "secondary-dark": "#A3B18A",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
